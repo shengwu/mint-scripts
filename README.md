1428b6d6b4fcb7e83db01519c054d8a65461da0f
Sheng Wu <pkernoobie@gmail.com>
2016-01-10 23:09:54 -0800

    summarize year script

---
+++ README.md
@@ -0,0 +1,21 @@
+# Mint scripts
+
+Development of Intuit's mint.com has stagnated for a while and the site still
+lacks simple filtering tools. I haven't found a site that has Mint's
+semi-automated transaction classification with better data analysis, so it
+looks like the best I can do is analyze the data myself.
+
+## summarize_year.py
+
+Log in to Mint and visit
+`https://wwws.mint.com/transaction.event?startDate=01/01/2015&endDate=12/31/2015`
+to get a year's worth of transactions. Click "Export all NNNN transactions" to
+download a CSV file.
+
+After you've downloaded your transactions, you can run:
+```
+python process_year.py transactions.csv
+```
+to get a quick visual summary of your spending. Some of the hardcoded
+categories are based on my custom labels in Mint, so you might have to
+edit them.
---
+++ summarize_year.py
@@ -0,0 +1,82 @@
+from collections import defaultdict
+import calendar
+import csv
+import copy
+import datetime
+
+categories = [
+    ['Air Travel'],
+    ['Alcohol'],
+    ['Books'],
+    ['Clothing'],
+    ['Coffee'],
+    ['Entertainment', 'Concerts', 'Music', 'Museums', 'Arts'],
+    ['Groceries'],
+    ['Gym'],
+    ['Paycheck'],
+    ['Public Transportation'],
+    ['Rental Car'],
+    ['Fast Food'],
+    ['Restaurants'],
+    ['Groceries', 'Fast Food', 'Restaurants'],
+    ['Rideshare', 'Taxi'],
+    ['Shopping', 'Electronics', 'Sporting Goods'],
+]
+
+def parse_transaction_list():
+  transactions = []
+  with open('transactions_2015.csv') as f:
+    reader = csv.reader(f, delimiter=',')
+    next(reader) # skip headers
+    for row in reader:
+      # parse dates in column 0 and amounts in column 3
+      newrow = copy.deepcopy(row)
+      parts = row[0].split('/')
+      newrow[0] = datetime.date(int(parts[2]), int(parts[0]), int(parts[1]))
+      newrow[3] = float(newrow[3])
+      transactions.append(newrow)
+  return transactions
+
+def matches(transaction, words):
+  for word in words:
+    if word in transaction[5]:
+      return True
+  return False
+
+def get_by_month_for_category(transactions, category_wordlist):
+  # Month -> spend that month
+  month_dict = defaultdict(float)
+  for transaction in transactions:
+    if matches(transaction, category_wordlist):
+      month_dict[transaction[0].month] += transaction[3]
+  for i in xrange(1, 13):
+    if i not in month_dict:
+      month_dict[i] = 0
+  return month_dict
+
+def print_month_dict(month_dict):
+  total = sum(month_dict.values())
+  divisor = total / 100
+  for month, cost in month_dict.iteritems():
+    print '%s $%.02f\t%s' % (calendar.month_name[month][:3], cost,
+        '#'*int(cost/divisor))
+
+transactions = parse_transaction_list()
+for category in categories:
+  month_dict = get_by_month_for_category(transactions, category)
+  print ', '.join(category)
+  print_month_dict(month_dict)
+  print 'TOTAL: $%.02f' % (sum(month_dict.values()),)
+  print
+  print
+
+
+credit = 0
+debit = 0
+for transaction in transactions:
+  if transaction[4] == 'debit':
+    debit += transaction[3]
+  else:
+    credit += transaction[3]
+print 'ALL CREDITS: $%.02f' % (credit, )
+print 'ALL DEBITS: $%.02f' % (debit, )
